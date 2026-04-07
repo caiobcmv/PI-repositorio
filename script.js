003eb94c4d12d8b4de27de6276c5729fc90e2b2a
caio barreto <caiobarretovalle269@outlook.com>
2026-04-07 17:09:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function acessarPortal() {
+    // Captura qual rádio está selecionado
+    const perfilSelecionado = document.querySelector('input[name="perfil"]:checked').value;
+    
+    // Lógica de redirecionamento baseada no perfil
+    if (perfilSelecionado === 'aluno') {
+        window.location.href = 'Dasboard.html'; // Crie esta página depois
+    } else if (perfilSelecionado === 'coordenador' || perfilSelecionado === 'secretaria') {
+        window.location.href = 'relatorios.html';
+    } else if (perfilSelecionado === 'super-adm') {
+        window.location.href = 'superadmin.html'; // Crie esta página depois
+    } else {
+        alert("Por favor, selecione um perfil.");
+    }
+}
